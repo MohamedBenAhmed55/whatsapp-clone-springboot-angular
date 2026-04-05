d9f8d690d37e75602e4bf83a9231824cb7388c82
MohamedBenAhmed55 <mohamed.benahmed@ensi-uma.tn>
2026-04-05 21:51:18 +0100

    [WTC] Creating named queries for User, Message and Chat

--- whatsappclone/src/main/java/com/mohamed/whatsappclone/chat/Chat.java
+++ whatsappclone/src/main/java/com/mohamed/whatsappclone/chat/Chat.java
@@ -20,6 +20,10 @@ import java.util.List;
 @NoArgsConstructor
 @Entity
 @Table(name = "chat")
+@NamedQuery(name = ChatConstants.FIND_CHAT_BY_SENDER_ID,
+            query = "SELECT DISTINCT c FROM Chat c WHERE c.sender.id = :senderId OR c.recipient.id = :senderId ORDER BY createdDate DESC ")
+@NamedQuery(name = ChatConstants.FIND_CHAT_BY_SENDER_ID_AND_RECEIVER_ID,
+            query = "SELECT DISTINCT c FROM Chat c WHERE (c.sender.id = :senderId and c.recipient.id = :recipientId) OR (c.sender.id = :recipientId and c.recipient.id = :senderId)")
 public class Chat extends BaseAuditingEntity {
 
     @Id
---
+++ whatsappclone/src/main/java/com/mohamed/whatsappclone/chat/ChatConstants.java
@@ -0,0 +1,9 @@
+package com.mohamed.whatsappclone.chat;
+
+public class ChatConstants {
+
+    public static final String FIND_CHAT_BY_SENDER_ID = "Chat.findCHatBySenderId";
+    public static final String FIND_CHAT_BY_SENDER_ID_AND_RECEIVER_ID = "Chat.findCHatBySenderIdAndReceiverId";
+
+    private ChatConstants() {}
+}
