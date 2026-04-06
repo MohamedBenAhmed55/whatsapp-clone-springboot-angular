3f933e78ce45682bdd52010328bccfaced953e58
MohamedBenAhmed55 <mohamed.benahmed@ensi-uma.tn>
2026-04-06 19:41:36 +0100

    [WTC][Feature] Chat Domain: Controller, Service, repo, mapper, endpoints response wrappers

---
+++ whatsappclone/src/main/java/com/mohamed/whatsappclone/chat/ChatMapper.java
@@ -0,0 +1,27 @@
+package com.mohamed.whatsappclone.chat;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatMapper {
+
+    private final ChatRepository chatRepository;
+
+    public ChatMapper(ChatRepository chatRepository) {
+        this.chatRepository = chatRepository;
+    }
+
+    public ChatResponse toChatResponse(Chat chat, String senderId) {
+        return ChatResponse.builder()
+                .id(chat.getId())
+                .name(chat.getChatName(senderId))
+                .unreadCount(chat.getUnreadMessageCount(senderId))
+                .lastMessage(chat.getLastMessage())
+                .isRecipientOnline(chat.getRecipient().isUserOnline())
+                .senderId(chat.getSender().getId())
+                .receiverId(chat.getRecipient().getId())
+                .build();
+    }
+
+
+  }
---
+++ whatsappclone/src/main/java/com/mohamed/whatsappclone/common/common/StringResponse.java
@@ -0,0 +1,13 @@
+package com.mohamed.whatsappclone.common.common;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StringResponse {
+    private String response;
+
+}
