4283fd673759067fee1bff4bbdb105d30871543d
MohamedBenAhmed55 <mohamed.benahmed@ensi-uma.tn>
2026-04-12 20:40:32 +0100

    [WTC][DevTool] Adding OpenAPI (Swagger UI)

--- whatsappclone/src/main/java/com/mohamed/whatsappclone/chat/ChatController.java
+++ whatsappclone/src/main/java/com/mohamed/whatsappclone/chat/ChatController.java
@@ -1,6 +1,7 @@
 package com.mohamed.whatsappclone.chat;
 
 import com.mohamed.whatsappclone.common.common.StringResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -11,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/va/chats")
 @RequiredArgsConstructor
+@Tag(name = "Chat")
 public class ChatController {
 
     private final ChatService chatService;
