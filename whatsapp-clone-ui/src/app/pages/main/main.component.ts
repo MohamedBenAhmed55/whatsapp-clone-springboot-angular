60db760d5d045660b7f9f32074a50164112bd31c
MohamedBenAhmed55 <mohamed.benahmed@ensi-uma.tn>
2026-05-03 00:37:01 +0100

    [WTC][Frontend] add the select contact logic to create a new chat, some logic fixes and Implementing the websocket

--- whatsapp-clone-ui/src/app/pages/main/main.component.ts
+++ whatsapp-clone-ui/src/app/pages/main/main.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ChatListComponent} from '../../components/chat-list/chat-list.component';
 import {ChatResponse} from '../../services/models/chat-response';
 import {ChatService} from '../../services/services/chat.service';
@@ -10,6 +10,10 @@ import {PickerComponent} from '@ctrl/ngx-emoji-mart';
 import {FormsModule} from '@angular/forms';
 import {EmojiData} from '@ctrl/ngx-emoji-mart/ngx-emoji';
 import {MessageRequest} from '../../services/models/message-request';
+import * as Stomp from 'stompjs';
+import SockJS from 'sockjs-client'
+import {Notification} from './notification';
+import * as console from 'node:console';
 
 @Component({
   selector: 'app-main',
@@ -18,13 +22,15 @@ import {MessageRequest} from '../../services/models/message-request';
   standalone: true,
   styleUrl: './main.component.scss'
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, OnDestroy {
 
   chats: Array<ChatResponse> = []
   selectedChat: ChatResponse = {};
   chatMessages: MessageResponse[] = [];
   showEmojis: boolean = false;
   messageContent: string = '';
+  socketClient: any = null;
+  private notificationSubscription: any;
 
   constructor(
     private chatService: ChatService,
@@ -32,7 +38,18 @@ export class MainComponent implements OnInit {
     private messageService: MessageService
   ) {
   }
+
+  ngOnDestroy(): void {
+    if (this.socketClient != null) {
+      this.socketClient.disconnect();
+      this.notificationSubscription.unsubscribe();
+      this.socketClient = null;
+    }
+  }
+
+
   ngOnInit() {
+    this.initWebSocket();
     this.getAllChats();
   }
 
@@ -140,4 +157,77 @@ export class MainComponent implements OnInit {
     }
     return this.selectedChat.senderId as string;
   }
+
+  private initWebSocket() {
+    if (this.keycloakService.keycloak.tokenParsed?.sub) {
+      let ws = new SockJS('http://localhost:8080/ws');
+      this.socketClient = Stomp.over(<WebSocket>ws);
+      const subUrl = `/user/${this.keycloakService.keycloak.tokenParsed?.sub}/chat`;
+      this.socketClient.connect({'Authorization': `Bearer ${ 'Bearer ' + this.keycloakService.userId}`},
+        () => {
+        this.notificationSubscription = this.socketClient.subscribe(subUrl,
+          (message: any ) => {
+            const notification: Notification = JSON.parse(message.body);
+
+            this.handleNotification(notification);
+          },
+          () => console.error('Error while connection to webSocket'));
+
+        }
+
+        );
+    }
+  }
+
+  private handleNotification(notification: Notification) {
+    if(!notification) return;
+    if(this.selectedChat && this.selectedChat.id === notification.chatId) {
+      switch(notification.type){
+        case 'MESSAGE':
+        case 'IMAGE':
+          const message: MessageResponse = {
+            senderId: notification.senderId,
+            receiverId: notification.receiverId,
+            content: notification.content,
+            type: notification.messageType,
+            media: notification.media,
+            createdAt: new Date().toString()
+          };
+          if (notification.type === 'IMAGE'){
+            this.selectedChat.lastMessage = 'Attachment';
+          } else {
+            this.selectedChat.lastMessage = notification.content;
+          }
+          this.chatMessages.push(message);
+          break;
+
+        case 'SEEN':
+          this.chatMessages.forEach(m => m.state = 'SEEN');
+          break;
+      }
+    } else {
+      const destChat = this.chats.find(c => c.id === notification.chatId);
+      if (destChat && notification.type !== 'SEEN') {
+        if(notification.type === 'MESSAGE'){
+          destChat.lastMessage = notification.content;
+        } else if (notification.type === 'IMAGE'){
+          destChat.lastMessage = 'Attachment';
+        }
+        destChat.lastMessageTime = new Date().toString();
+        destChat.unreadCount! += 1;
+      } else if (notification.type === 'MESSAGE') {
+        const newChat: ChatResponse = {
+          id: notification.chatId,
+          senderId: notification.senderId,
+          receiverId: notification.receiverId,
+          lastMessage: notification.content,
+          name: notification.chatName,
+          unreadCount: 1,
+          lastMessageTime: new Date().toString(),
+        }
+        this.chats.unshift(newChat);
+      }
+    }
+  }
+
 }
--- whatsappclone/src/main/java/com/mohamed/whatsappclone/chat/ChatController.java
+++ whatsappclone/src/main/java/com/mohamed/whatsappclone/chat/ChatController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/va/chats")
+@RequestMapping("/api/v1/chats")
 @RequiredArgsConstructor
 @Tag(name = "Chat")
 public class ChatController {
